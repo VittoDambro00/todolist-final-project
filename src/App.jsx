68421ffba173fe73968d10e350dbc28e6ba36395
VittoDambro00 <vittodambro00@gmail.com>
2023-12-14 12:35:10 +0100

    aggiunta router

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 export default function App() {
- return <div></div>
+return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
