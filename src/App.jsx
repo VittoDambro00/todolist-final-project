a863eb127325d6aa22aa551cef48e5235ed42ef4
VittoDambro00 <vittodambro00@gmail.com>
2023-12-14 12:31:47 +0100

    Aggiunta router in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx"
 
 export default function App() {
- return <div></div>
+  return (
+    <div>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
