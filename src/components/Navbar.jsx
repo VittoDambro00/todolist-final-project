a4f3d2e5c47b0d1d74cd0c2d201fac9ae33e6348
Riccardo Patronaggio <riccardo.patronaggio@studium.unict.it>
2023-12-14 17:59:56 +0100

    modifiche finali giorno 1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav className="flex flex-row justify-between items-center bg-[#E25858]/90 h-[80px] p-4 font-medium text-white w-full sticky top-0 left-0">
+    <nav className="flex flex-row justify-between items-center bg-[#E25858]/90 h-[80px] p-4 font-medium text-white w-full  top-0 left-0">
       <div className="flex flex-row justify-center items-center gap-4">
         <img src="https://freesvg.org/img/pomodorotimer.png" alt="logo" className="h-[70px] w-[70px]" />
         <h1>To-Do List</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,37 @@
 import { useState } from "react";
+import img from "../assets/img/Clipboard.png";
 
 export default function Home() {
   const [lista, setLista] = useState([]);
+  const [firstTask, setFirstTask] = useState(false);
   const [nuovaTask, setTask] = useState("");
+  const [singolTask, setsingolTask] = useState(0);
 
   function controllaCambio(event) {
     setTask(event.target.value);
   }
 
   function aggiungiTask() {
+    console.log(firstTask);
+    setFirstTask(true);
     const task = {
       id: lista.length === 0 ? 1 : lista[lista.length - 1].id + 1,
       taskName: nuovaTask,
     };
+    setsingolTask(singolTask + 1);
     const newArray = [...lista, task];
     setLista(newArray);
   }
 
   function elimaTask(id) {
-    setLista(lista.filter((task) => task.id !== id));
+     setLista(lista.filter((task) => task.id !== id));
+     setsingolTask(singolTask - 1);
   }
+  console.log(firstTask);
 
   return (
-    <div className="bg-[#1A1A1A] h-[calc(100vh-80px)]">
-      <div className="h-[50vh] flex justify-center items-center gap-[8px]">
+    <div className="bg-[#1A1A1A] min-h-[calc(100vh-80px)] flex justify-center items-center flex-col">
+      <div className="h-[25vh] flex justify-center items-center gap-[8px] mt-[100px]">
         <input
           className="w-[639px] h-[54px] rounded-[8px] p-[16px] gap-[8px] bg-[#262626] hover:border-0 text-white"
           type="text"
@@ -36,9 +44,32 @@ export default function Home() {
           Invio
         </button>
       </div>
-      <div className="text-white">ciao</div>
 
-      <div className="h-[50vh] flex justify-center items-center flex-col">
+      <div className="w-[736px] flex justify-center items-center flex-col mb-[100px]">
+        <div className="flex justify-between text-white w-[736px] mt-[100px] mb-[100px]">
+          <div className="h-0 text-[#E25858]">
+            Task created{" "}
+            <span className="bg-slate-600 p-[3px] rounded-full text-white">
+              {singolTask}
+            </span>
+          </div>
+          <div className="h-0 text-[#E25858]">
+            <span className="bg-slate-600 p-[3px] rounded-full text-white">
+              0
+            </span>{" "}
+            Tasks completed
+          </div>
+        </div>
+        {!firstTask && (
+          <div className="h-[25vh] flex justify-center items-center flex-col gap-3 text-[#808080] text-center">
+            <img src={img} alt="clip-icon" className="h-[56px] w-[56px]" />
+            <p>
+              Non hai ancora attività registrate <br /> Crea attività e
+              organizza le tue cose da fare
+            </p>
+          </div>
+        )}
+
         {lista.map((task, index) => {
           return (
             <div
