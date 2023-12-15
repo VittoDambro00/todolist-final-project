3ac1924dfdde3230b8dfe3c81b8d734aa62d3dc2
OodDev <polthepol@gmail.com>
2023-12-15 11:16:51 +0100

    Fixato file App.jsx come in main

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.jsx";
 import WelcomeScreen from "./pages/WelcomeScreen.jsx";
 import Error404 from "./pages/Error404.jsx";
 import Footer from "./components/Footer.jsx";
 import Home from "./pages/Home.jsx";
-import PomodoroTimer from "./pages/PomodoroTimer"; 
+import PomodoroTimer from "./pages/PomodoroTimer";
 
 export default function App() {
   return (
@@ -21,4 +22,3 @@ export default function App() {
     </div>
   );
 }
-
