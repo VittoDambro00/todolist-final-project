ae9407d33c755b065b64d015d0ee047611760fa6
OodDev <polthepol@gmail.com>
2023-12-15 14:45:20 +0100

    fix coments, welcome screen

--- src/pages/WelcomeScreen.jsx
+++ src/pages/WelcomeScreen.jsx
@@ -5,31 +5,31 @@ import { Link } from "react-router-dom"
 
 
 // Imposto palette colori, dei 2 temi.
-/* const coloreTitolo = "text-sky-50";
-const coloreTesto = "text-sky-100";
-const coloreSfondo = "bg-sky-950";
-const paletteColoriDark = {titolo: coloreTitolo, testo: coloreTesto, sfondo: coloreSfondo};
-const coloreTitoloLight = "text-sky-950";
-const coloreTestoLight = "text-sky-700";
-const coloreSfondoLight = "bg-sky-50";
-const paletteColoriLight = {titolo: coloreTitoloLight, testo:coloreTestoLight, sfondo:coloreSfondoLight};
-let darkThemeOn = true; */
+// const coloreTitolo = "text-sky-50";
+// const coloreTesto = "text-sky-100";
+// const coloreSfondo = "bg-sky-950";
+// const paletteColoriDark = {titolo: coloreTitolo, testo: coloreTesto, sfondo: coloreSfondo};
+// const coloreTitoloLight = "text-sky-950";
+// const coloreTestoLight = "text-sky-700";
+// const coloreSfondoLight = "bg-sky-50";
+// const paletteColoriLight = {titolo: coloreTitoloLight, testo:coloreTestoLight, sfondo:coloreSfondoLight};
+// let darkThemeOn = true;
 
 // Funzione che cambia tema
 export default function WelcomeScreen() {
-/*     
-    const [paletteColori, setPaletteColori] = useState(paletteColoriDark);
-    function cambiaPaletteColori() {
-        darkThemeOn = !darkThemeOn;
-        darkThemeOn ? setPaletteColori(paletteColoriDark) : setPaletteColori(paletteColoriLight);   
-    } */
+    
+    // const [paletteColori, setPaletteColori] = useState(paletteColoriDark);
+    // function cambiaPaletteColori() {
+    //     darkThemeOn = !darkThemeOn;
+    //     darkThemeOn ? setPaletteColori(paletteColoriDark) : setPaletteColori(paletteColoriLight);   
+    // } a
 
     
-    /*     function gestisciPressioneInvio(bottonePremuto) {
-        if (bottonePremuto.key === "Enter") {
-            vaiAHome();
-        }
-    } */
+    //     function gestisciPressioneInvio(bottonePremuto) {
+    //     if (bottonePremuto.key === "Enter") {
+    //         vaiAHome();
+    //     }
+    // }
     const [nomeUtente, setNomeUtente] = useState("Guest");
     
     useEffect(() => {
@@ -61,7 +61,7 @@ export default function WelcomeScreen() {
                                 required 
                             />
 
-{/*                             <button onClick={()=>{
+                            {/* <button onClick={()=>{
                                 setNomeUtente(nomeUtente)
                                 useNavigate().navigateTo("/index")
                             }}>Inizia!</button> */}
